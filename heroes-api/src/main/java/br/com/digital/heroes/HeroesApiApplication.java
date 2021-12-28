bd1df2ba0496581ae7c68e8ebc0bf9eb0b482550
Paulo Diniz <paullohdiniz@gmail.com>
2021-12-28 16:59:45 -0300

    Primeiro input github

---
+++ heroes-api/src/main/java/br/com/digital/heroes/HeroesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.digital.heroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesApiApplication.class, args);
+	}
+
+}
